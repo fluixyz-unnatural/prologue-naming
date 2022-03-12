ef22f482d633ad4c4413813b66c0bbac5dc6efb4
fluixyz-unnatural <sudo3949@gmail.com>
2022-03-12 12:23:23 +0900

    配列からの選択、randomにあった

--- generator.py
+++ generator.py
@@ -9,16 +9,12 @@ seis = []
 with open('sei_uniq.csv','r') as f:
     seis = f.read().split('\n')[:-1]
 
-def random_pick (arr):
-    i = random.randint(0,len(arr)-1)
-    return arr[i]
-
 def generate():
     name_len = random.randint(1,2)
     name = ""
     for i in range(name_len):
-        name += random_pick(jinmei_kanji)
-    sei = random_pick(seis)
+        name += random.choice(jinmei_kanji)
+    sei = random.choice(seis)
     return (sei+' '+ name)
 
 def main():
